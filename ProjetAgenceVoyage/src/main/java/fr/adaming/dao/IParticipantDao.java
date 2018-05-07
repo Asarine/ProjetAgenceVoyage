c01ca28566a50dae81c5a7136fb569b52056a61e
inti0343 <inti0343@DESKTOP-KI42TAP>
2018-05-07 18:17:52 +0200

    Fab - sinon ça compile pas

--- ProjetAgenceVoyage/src/main/java/fr/adaming/dao/IParticipantDao.java
+++ ProjetAgenceVoyage/src/main/java/fr/adaming/dao/IParticipantDao.java
@@ -5,13 +5,13 @@ import java.util.List;
 import fr.adaming.model.Participant;
 
 public interface IParticipantDao {
-	public int addParticipant(Participant p);
-
-	public int updateParticipant(Participant p);
-
-	public int deleteParticipant(Long id);
-
-	public Participant searchParticipant(Long id);
-
-	public List<Participant> getAllParticipant();
+//	public int addParticipant(Participant p);
+//
+//	public int updateParticipant(Participant p);
+//
+//	public int deleteParticipant(Long id);
+//
+//	public Participant searchParticipant(Long id);
+//
+//	public List<Participant> getAllParticipant();
 }
