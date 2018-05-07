cd6159bbfa9bd49f4127077785d8543b511be957
inti0349 <inti0349@DESKTOP-GVCD5OB>
2018-05-07 16:57:00 +0200

    Elod : boolean annot

--- ProjetAgenceVoyage/src/main/java/fr/adaming/model/Hebergement.java
+++ ProjetAgenceVoyage/src/main/java/fr/adaming/model/Hebergement.java
@@ -3,6 +3,7 @@ package fr.adaming.model;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -23,8 +24,11 @@ public class Hebergement implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id_h;
+	@Column(columnDefinition= "TINYINT(1)")
 	private boolean pdj;
+	@Column(columnDefinition= "TINYINT(1)")
 	private boolean dj;
+	@Column(columnDefinition= "TINYINT(1)")
 	private boolean diner;
 
 	// Transformation association
