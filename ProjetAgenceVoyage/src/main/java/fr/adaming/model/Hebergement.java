a76df76462524dcc631a80de72bfcc622c197eec
inti0349 <inti0349@DESKTOP-GVCD5OB>
2018-05-09 11:46:14 +0200

    Wyndy : Hergement

--- ProjetAgenceVoyage/src/main/java/fr/adaming/model/Hebergement.java
+++ ProjetAgenceVoyage/src/main/java/fr/adaming/model/Hebergement.java
@@ -1,1 +1,101 @@
+package fr.adaming.model;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "hebergements")
+public class Hebergement implements Serializable {
+
+	/**
+	*
+	*/
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_h;
+	// @Column(columnDefinition= "TINYINT(1)")
+	// private boolean pdj;
+	// @Column(columnDefinition= "TINYINT(1)")
+	// private boolean dj;
+	// @Column(columnDefinition= "TINYINT(1)")
+	// private boolean diner;
+	private String selectionhbg;
+
+	// Transformation association
+	@OneToMany(mappedBy = "hebergement")
+	List<Voyage> listevoyage;
+	@OneToMany(mappedBy = "hebergement")
+	List<Image> listeimage;
+
+	// Constructeurs
+
+	public Hebergement() {
+		super();
+	}
+
+	public Hebergement(long id_h, String selectionhbg) {
+		super();
+		this.id_h = id_h;
+		this.selectionhbg = selectionhbg;
+	}
+
+	public Hebergement(String selectionhbg) {
+		super();
+		this.selectionhbg = selectionhbg;
+	}
+
+	//G+S
+	public long getId_h() {
+		return id_h;
+	}
+
+	public void setId_h(long id_h) {
+		this.id_h = id_h;
+	}
+
+	public String getSelectionhbg() {
+		return selectionhbg;
+	}
+
+	public void setSelectionhbg(String selectionhbg) {
+		this.selectionhbg = selectionhbg;
+	}
+
+	public List<Voyage> getListevoyage() {
+		return listevoyage;
+	}
+
+	public void setListevoyage(List<Voyage> listevoyage) {
+		this.listevoyage = listevoyage;
+	}
+
+	public List<Image> getListeimage() {
+		return listeimage;
+	}
+
+	public void setListeimage(List<Image> listeimage) {
+		this.listeimage = listeimage;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Hebergement [id_h=" + id_h + ", selectionhbg=" + selectionhbg + ", listevoyage=" + listevoyage
+				+ ", listeimage=" + listeimage + "]";
+	}
+
+
+	
+}
