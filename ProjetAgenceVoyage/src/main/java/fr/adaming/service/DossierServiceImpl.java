3a530ab449d163f064ff11708293982c4c546bea
inti0343 <inti0343@DESKTOP-KI42TAP>
2018-05-14 09:07:07 +0200

    Fab : regles gestion dossier

--- ProjetAgenceVoyage/src/main/java/fr/adaming/service/DossierServiceImpl.java
+++ ProjetAgenceVoyage/src/main/java/fr/adaming/service/DossierServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import fr.adaming.dao.IDossierDao;
 import fr.adaming.model.Dossier;
+import fr.adaming.util.MailSendPdf;
 
 @Service
 @Transactional
@@ -15,11 +16,23 @@ public class DossierServiceImpl implements IDossierService {
 	
 	@Autowired
 	private IDossierDao dossierDao;
+	
+	private MailSendPdf smpdf;
 
 	@Override
 	public Dossier addDossier(Dossier dos) {
-		
-		return dossierDao.addDossier(dos);
+		// Le dossier doit être examiné par Bovoyage
+		dos.setStatutdossier("En attente");
+
+		Dossier dOut = dossierDao.addDossier(dos);
+
+		// Informer le client et le client
+		if (dOut.getId() != 0) {
+			smpdf.sendMail2Client(dos);
+			smpdf.sendMail2Conseiller(dos);
+		}
+
+		return dOut;
 	}
 
 	@Override
@@ -30,8 +43,29 @@ public class DossierServiceImpl implements IDossierService {
 
 	@Override
 	public int updateDossier(Dossier dos) {
-		
-		return dossierDao.updateDossier(dos);
+		Dossier dOld = dossierDao.getDossierById(dos.getId());
+
+		int verif = dossierDao.updateDossier(dos);
+
+		if (verif != 0) {
+			if (dOld.getStatutdossier() != dos.getStatutdossier()) {
+
+				// Si le dossier est validé, générer le PDF
+				if (dos.getStatutdossier() == "Accepté") {
+					smpdf.genererPDF(dos);
+				}
+
+				// Prévenir le client de la modification
+				smpdf.sendMail2Client(dos);
+			}
+		}
+
+		// Si le dossier ne peut être accepté
+		if (dos.getStatutdossier() == "Annulé" || dos.getStatutdossier() == "Refusé") {
+			verif = dossierDao.deleteDossier(dos.getId());
+		}
+
+		return verif;
 	}
 
 	@Override
