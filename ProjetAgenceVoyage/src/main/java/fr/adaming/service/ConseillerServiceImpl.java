d33e83cc3e2abc6e080f5c7bb794c693e9a0e878
inti0343 <inti0343@DESKTOP-KI42TAP>
2018-05-14 11:06:30 +0200

    Fab : oups...

--- ProjetAgenceVoyage/src/main/java/fr/adaming/service/ConseillerServiceImpl.java
+++ ProjetAgenceVoyage/src/main/java/fr/adaming/service/ConseillerServiceImpl.java
@@ -4,13 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import fr.adaming.dao.IConseillerDao;
 import fr.adaming.model.Conseiller;
 
 @Service
 @Transactional
 public class ConseillerServiceImpl implements IConseillerService {
 	@Autowired
-	private IConseillerService conseilDao;
+	private IConseillerDao conseilDao;
 
 	@Override
 	public Conseiller getConseillerById(long id) {
